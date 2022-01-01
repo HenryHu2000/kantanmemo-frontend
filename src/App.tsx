cc1e72cada4b31eb6065195c8d8f41c956a18e05
Henry Hu <henryhu2000@outlook.com>
2022-01-02 03:12:08 +0800

    Add file uploading

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File>();
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(event.target.files?.[0]);
+  };
+
+  const handleSubmission = () => {
+    if (selectedFile !== undefined) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      fetch(
+        // 'http://localhost:8080/upload',
+        'https://kantanmemo-backend.herokuapp.com/upload',
+        {
+          method: 'POST',
+          body: formData,
+        }
+      )
+        .then((response) => response.text())
+        .then((result) => {
+          console.log('Success:', result);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" name="file" onChange={changeHandler} />
+      {selectedFile ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
     </div>
   );
 }
