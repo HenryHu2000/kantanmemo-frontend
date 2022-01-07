27f43d0b416a735f251a5982c7c4fdaa9de91eea
Henry Hu <henryhu2000@outlook.com>
2022-01-07 16:58:46 +0800

    Change backend URL to kantan-api.skygreen.org

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,7 @@
 // export const BACKEND_URL = 'http://localhost:8080';
 // export const COOKIE_DOMAIN = 'localhost';
 
-export const BACKEND_URL = 'http://kantanmemo-backend.skygreen.org';
+export const BACKEND_URL = 'http://kantan-api.skygreen.org';
 export const COOKIE_DOMAIN = '.skygreen.org';
 
 export const DEFAULT_DAILY_NEW_WORD_NUM = 10;
